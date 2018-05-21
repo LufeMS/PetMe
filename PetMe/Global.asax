2e6a08668ae880881a0200571f8f9bfb65e86e29
Luiz Felipe Matos <luizfelipematoss@gmail.com>
2018-05-21 11:24:43 -0300

    Adicionar arquivos de projeto.

---
+++ PetMe/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PetMe.MvcApplication" Language="C#" %>
---
+++ PetMe/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PetMe.Startup))]
+namespace PetMe
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
